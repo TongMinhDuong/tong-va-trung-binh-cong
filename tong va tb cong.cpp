189d1b57c55c8ac77d8b885b02072785c08d6c90
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-03-13 10:19:19 +0700

    Add files via upload

---
+++ tong va tb cong.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a = 24;
+	int b = 56;
+	int c = 72;
+	int d = 36;
+	int e = 82;
+	int sum = a+b+c+d+e;
+	int Avg = sum / 5;
+	
+	printf("sum = %d\n", sum);
+	printf("Avg = %d", Avg);
+	return 0;
+}
